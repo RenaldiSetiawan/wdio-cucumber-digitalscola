b6c5d7387a5d2324aeb84bd3860a5afd81971258
renaldisetiawan <enang.setiawan22@gmail.com>
2024-05-03 11:07:08 +0700

    change logic untuk step add to cart

--- saucedemo/pageobjects/Inventory.page.js
+++ saucedemo/pageobjects/Inventory.page.js
@@ -3,26 +3,46 @@ import Page from "./page.js";
 
 class InventoryPage extends Page {
 
-  get addToCartButton() {
-    return $("#add-to-cart-sauce-labs-backpack");
-  };
+  async productList() {
+    await browser.url('https://www.saucedemo.com/inventory.html');
+  }
 
-  async addItemToCart() {
-    // console.log("Tambahkan ke Keranjang");
-    await this.addToCartButton.click();
+  async clikButton() {
+    await $('//*[@data-test="add-to-cart-sauce-labs-backpack"]').click();
 
-    // console.log("wait ke halaman keranjang");
-    await browser.waitUntil(
-        async () => {
-            return browser.getUrl() !== "https://www.saucedemo.com/inventory.html";
-        },
-        {
-            timeout: 10000,
-            timeoutMsg: "Gagal menunggu navigasi ke halaman keranjang",
-        }
-    );
+    await browser.waitUntil(async () => {
+      return browser.getUrl() !== "https://www.saucedemo.com/inventory.html";
+    }, {
+      timeout: 10000,
+      timeoutMsg: "Gagal menunggu navigasi ke halaman keranjang",
+    });
+  }
 
-    await browser.url("https://www.saucedemo.com/cart.html");
+  validateItemOnCart() {
+    browser.url('https://www.saucedemo.com/cart.html');
+
+    const cartItemXPath = '//*[@data-test="inventory-item"]';
+
+    browser.waitUntil(() => {
+      const cartItem = $(cartItemXPath);
+      return cartItem.isDisplayed();
+    }, {
+      timeout: 5000,
+      timeoutMsg: 'Item tidak berhasil ditambahkan ke keranjang dalam 5 detik'
+    });
+
+    // Memeriksa apakah item berhasil ditambahkan ke keranjang
+    const cartItem = $(cartItemXPath);
+    const isDisplayed = cartItem.isDisplayed();
+
+    let itemName = '';
+    if (isDisplayed) {
+      itemName = cartItem.getText();
+    }
+
+    console.log(`Item '${itemName}' ${isDisplayed ? 'berhasil' : 'gagal'} ditambahkan ke keranjang`);
+
+    return cartItem.isDisplayed();
   }
 };
 
