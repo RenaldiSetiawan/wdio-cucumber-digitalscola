96b9c6b181a631671ffcdddd22bf6498b357fc3e
renaldisetiawan <enang.setiawan22@gmail.com>
2024-05-03 11:08:01 +0700

    use sreenshot and allure report

--- saucedemo/step-definitions/step-saucedemo.js
+++ saucedemo/step-definitions/step-saucedemo.js
@@ -2,6 +2,7 @@ import { Given, When, Then } from '@wdio/cucumber-framework';
 import { expect, $ } from '@wdio/globals';
 import chai from 'chai';
 import chaiAsPromised from 'chai-as-promised';
+import allureReporter from '@wdio/allure-reporter';
 
 import LoginPage from '../pageobjects/login.page.js';
 import InventoryPage from '../pageobjects/Inventory.page.js';
@@ -14,11 +15,13 @@ const pages = {
     inventory: InventoryPage
 };
 
+/* Login Step Start */
 // STEP USER SUCCESS LOGIN
 Given(/^User is on the Saucedemo (\w+) page$/, async (page) => {
     await pages[page].open()
 });
 
+// User successfully login with <username> and <password>
 When(/^User successfully login with (\w+) and (.+)$/, async (username, password) => {
     await LoginPage.login(username, password)
 });
@@ -29,19 +32,27 @@ Then(/^User is on the Saucedemo dashboard$/, async () => {
     assert.isTrue(isDashboardVisible, 'Dashboard is not visible');
 });
 
-// Add to cart Step Start
-When(/^User adds Button to Cart$/, async () => {
-    await InventoryPage.addItemToCart();
+/* Add To Cart Step Start */
+// User berada di halaman Produk
+Given(/^User is on the product page$/, async () => {
+    await InventoryPage.productList();
 });
 
-// Validate item sukses ditambahkan ke cart
-Then(/^Item successfully added to cart page$/, async () => {
-    const cartItems = await $$('//*[@data-test="inventory-item"]');
-    assert.isNotEmpty(cartItems, 'No items are displayed in the cart');
+// User Clik Button add to cart
+When(/^User adds item to cart$/, async () => {
+    await InventoryPage.clikButton();
+});
 
-    // Menampilkan nama-nama item yang telah ditambahkan ke keranjang
-    for (const item of cartItems) {
-        const itemName = await item.$('//*[@data-test="inventory-item"]');
-        console.log("Item in cart:", await itemName.getText());
-    }
+// Item yang di klik masuk ke cart
+Then(/^Item is successfully added to cart$/, async () => {
+    const isItemAdded = await InventoryPage.validateItemOnCart();
+    assert.isTrue(isItemAdded, 'Item should be successfully added to cart');
+
+    // Menyimpan tangkapan layar dari browser
+    const chromeScreenshotPath = './saucedemo/saveScreenshot/ValidateCartItem.png';
+    await browser.saveScreenshot(chromeScreenshotPath);
+
+    // Use Allure
+    allureReporter.addFeature('Saucedemo');
+    allureReporter.addStory('Add item to cart and Validate item successfully added to cart');
 });
